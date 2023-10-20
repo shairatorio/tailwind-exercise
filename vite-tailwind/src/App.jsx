b14804ea031cec3fcbad622bd6f21dad3aaf1802
shairatorio <shairatorio08@gmail.com>
2023-10-20 16:22:27 +0800

    vite tailwindcss practice

---
+++ vite-tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <section className="h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl
+      p-12 md:p-14 lg:p-16 xl:p-20">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi totam
+          animi reprehenderit voluptatum mollitia vel cum sapiente, repudiandae
+          atque sed alias ducimus, ad quas quo assumenda? Dolorem est at
+          inventore.
+        </p>
+      </section> */}
+      <div className="card rounded-none">This is a card.</div>
+    </div>
+  );
+};
+
+export default App;
